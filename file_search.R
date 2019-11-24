c58622a45f6402958fd285896ba3625ce98a6ca3
harryahlas <42125399+harryahlas@users.noreply.github.com>
2019-11-23 17:22:18 -0800

    update

--- file_search.R
+++ file_search.R
@@ -2,23 +2,27 @@
 
 library(tidyverse)
 
-# Identify search terms
-search_terms <- c("hrsample", "HRSAMPLE")
-search_terms_re <- paste0(search_terms, collapse = "|")
+# Identify directory to search
+directory_to_search <- "C:\\Development\\github\\blog"#server-python"
 
 # Identify filetypes to search
 extension_types <- c(".html", ".R")
-extension_types_re <- paste0(extension_types, "$", collapse = "|")
+extension_types_re <- paste0(extension_types, "$", collapse = "|") # format for regex filter
+
+# Identify search terms
+search_terms <- c("hrsample", "HRSAMPLE")
+search_terms_re <- paste0(search_terms, collapse = "|") # format for regex filter
+
+# Identify files to search
+files_to_search <- list.files(directory_to_search, recursive = TRUE) %>% 
+  tibble(filenames = .) %>% 
+  filter(grepl(extension_types_re, filenames, ignore.case = TRUE)) 
 
 
-directory_to_search <- "C:\\Development\\github\\blog"#server-python"
-
-file_list <- list.files(directory_to_search, recursive = TRUE)
 
 
 files_to_search <- tibble(
-  filenames = list.files(directory_to_search, recursive = TRUE)) %>% 
-  filter(grepl(extension_types_re, filenames, ignore.case = TRUE)) # filter for extensions
+  filenames = list.files(directory_to_search, recursive = TRUE))  filter for extensions
   
 
 search_terms <- c(".wav", ".mp3")
