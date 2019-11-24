a1457bc449f5354221ec6499144b3e22acda535c
harryahlas <42125399+harryahlas@users.noreply.github.com>
2019-11-23 17:03:57 -0800

    Update file_search.R

--- file_search.R
+++ file_search.R
@@ -1,5 +1,28 @@
+#Search for terms in specific filetypes
+
+library(tidyverse)
+
+# Identify search terms
+search_terms <- c("hrsample", "HRSAMPLE")
+search_terms_re <- paste0(search_terms, collapse = "|")
+
+# Identify filetypes to search
+extension_types <- c(".html", ".R")
+extension_types_re <- paste0(extension_types, "$", collapse = "|")
 
 
-directory_to_search <- "C:\\Development\\github\\"#server-python"
+directory_to_search <- "C:\\Development\\github\\blog"#server-python"
 
-list.files(directory_to_search, recursive = TRUE)
+file_list <- list.files(directory_to_search, recursive = TRUE)
+
+
+files_to_search <- tibble(
+  filenames = list.files(directory_to_search, recursive = TRUE)) %>% 
+  filter(grepl(extension_types_re, filenames, ignore.case = TRUE)) # filter for extensions
+  
+
+search_terms <- c(".wav", ".mp3")
+file_list[grepl("^m.*\\.log", file_list)]
+#file_list[grepl("*\\.wav$", file_list)]%>% length()
+
+file_list[grepl(".mp3$|.wav$", file_list)] 
